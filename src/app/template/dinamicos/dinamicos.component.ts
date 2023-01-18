001a8365438ad726e7a0f2b4050f780700b45f14
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-01-18 17:43:24 -0500

    template dinamicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: []
+  styles: [],
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+  nuevoJuego: string = '';
 
-  constructor() { }
+  persona: Persona = {
+    nombre: 'Jhonatan',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Metal Gear',
+      },
+      {
+        id: 2,
+        nombre: 'Dante Inferno',
+      },
+    ],
+  };
 
-  ngOnInit(): void {
+  agregarJuego(): void {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego,
+    };
+
+    this.persona.favoritos.push({ ...nuevoFavorito });
+    this.nuevoJuego = '';
   }
 
+  guardar(): void {
+    console.log('Formulario posteado');
+  }
+
+  eliminar(index: number): void {
+    this.persona.favoritos.splice(index, 1);
+  }
 }
