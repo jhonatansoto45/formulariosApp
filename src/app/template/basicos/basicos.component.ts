80a3c71998d686f43bdc6a0ccc97cfbccc3d0ed8
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-01-18 16:20:19 -0500

    template basicos

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: []
+  styles: [],
 })
 export class BasicosComponent implements OnInit {
+  @ViewChild('miFormulario') miFormulario!: NgForm;
 
-  constructor() { }
+  initForm = {
+    producto: 'RTX 4080ti',
+    precio: 10,
+    existencias: 10,
+  };
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  //* ALTERNATIVA
+  /* guardar(miFormulario: NgForm): void {
+    console.log(miFormulario);
+  } */
+
+  nombreValido(): boolean {
+    return (
+      this.miFormulario?.controls['producto']?.invalid &&
+      this.miFormulario?.controls['producto']?.touched
+    );
   }
 
+  precioValido(): boolean {
+    return (
+      this.miFormulario?.controls['precio']?.touched &&
+      this.miFormulario?.controls['precio']?.value <= 0
+    );
+  }
+
+  guardar(): void {
+    //console.log(this.miFormulario);
+    console.log('Posteo correcto');
+
+    //this.miFormulario.resetForm();// * LIMPIAR EL FORMULARIO
+    this.miFormulario.resetForm({
+      //* LIMPIAR EL FORMULARIO E INICIALIZA LOS CAMPOS
+      producto: 'Algo',
+      precio: 0,
+      existencias: 0,
+    });
+  }
 }
