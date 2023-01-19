643388929d3213c8a9efc2f3977c551522cf2a7e
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-01-19 10:29:37 -0500

    reactivo basico

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
   templateUrl: './basicos.component.html',
-  styles: [``]
+  styles: [``],
 })
 export class BasicosComponent implements OnInit {
+  /* //* INICIALIZA UN FORMGROUP Y FORMCONTROL
+  miFormulario: FormGroup = new FormGroup({
+    nombre: new FormControl('RTX 4080ti'), //* ITEM
+    precio: new FormControl(1500),
+    existencias: new FormControl(5),
+  }); */
 
-  constructor() { }
+  //* FORMBUILDER
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]], //* [valor, validador sincrono, validador asincrono],
+    precio: [, [Validators.required, Validators.min(0)]],
+    existencias: [, [Validators.required, Validators.min(0)]],
+  });
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.miFormulario.setValue({
+      nombre: 'RTX 4080ti',
+      precio: 1600,
+      existencias: 10,
+    });
   }
 
+  campoEsValido(campo: string) {
+    return (
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
+
+  guardar() {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched(); //* TOCA TODO LOS CAMPOS ACTIVANDO LOS ERRORES
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
 }
