2d27bf55b745e22c7eeab56104d486dd06ee4856
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-01-19 11:30:59 -0500

    reactivos dinamicos

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
-  styles: []
+  styles: [],
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array(
+      [
+        ['Metal Gear', Validators.required],
+        ['Dante Inferno', Validators.required],
+      ],
+      Validators.required
+    ),
+  });
 
-  constructor() { }
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {}
+
+  get favoritosArray() {
+    return this.miFormulario.get('favoritos') as FormArray;
   }
 
+  campoEsValido(campo: string): boolean | null {
+    return (
+      this.miFormulario.controls[campo].errors &&
+      this.miFormulario.controls[campo].touched
+    );
+  }
+
+  agregarFavorito() {
+    if (this.nuevoFavorito.invalid) return;
+
+    /* this.favoritosArray.push(
+      new FormControl(this.nuevoFavorito.value, Validators.required)
+    ); */
+
+    this.favoritosArray.push(
+      this.fb.control(this.nuevoFavorito.value, Validators.required)
+    );
+
+    this.nuevoFavorito.reset();
+  }
+
+  borrarFavorito(index: number) {
+    this.favoritosArray.removeAt(index);
+  }
+
+  guardar(): void {
+    if (this.miFormulario.invalid) {
+      this.miFormulario.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.miFormulario.value);
+    this.miFormulario.reset();
+  }
 }
