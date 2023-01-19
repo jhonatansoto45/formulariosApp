6152ca7659530eebc4e242461c59e4e36af2b6d3
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-01-19 09:28:21 -0500

    template switches

--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
   styles: [],
 })
-export class SwitchesComponent implements OnInit {
-  constructor() {}
+export class SwitchesComponent {
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  };
 
-  ngOnInit(): void {}
+  terminosYCondiciones: boolean = false;
 }
