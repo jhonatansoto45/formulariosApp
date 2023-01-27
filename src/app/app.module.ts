510ae7fcda05d0b54a64160d6f4a2b901aa74697
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-01-27 10:11:00 -0500

    Validaciones de formulario asincronas y vistas de errores mucho mas comoda de email

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveModule } from './reactive/reactive.module';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     AppRoutingModule,
     BrowserModule,
+    HttpClientModule,
     ReactiveModule,
     SharedModule,
     TemplateModule,
--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -5,6 +5,7 @@ import {
   FormGroup,
   Validators,
 } from '@angular/forms';
+import { EmailValidatorService } from 'src/app/shared/validator/email-validator.service';
 import { ValidatorService } from '../../../shared/validator/validator.service';
 
 @Component({
@@ -25,23 +26,44 @@ export class RegistroComponent implements OnInit {
       email: [
         '',
         [Validators.required, Validators.pattern(this.vs.emailPattern)],
+        [this.emailValidator],
       ],
       username: ['', [Validators.required, this.vs.noPuedeSerStrider]], //* PARA ENVIAR UN FUNCION SOLO SE ENVIA LA REFERENCIA (SIN () )
       password: ['', [Validators.required, Validators.minLength(6)]],
-      passwaor2: ['', [Validators.required]],
+      password2: ['', [Validators.required]],
     },
     //* VALIDACIONES DE TODO EL FORMULARIO
     { Validators: [this.vs.camposIguales('password', 'password2')] }
   );
 
-  constructor(private fb: FormBuilder, private vs: ValidatorService) {}
+  constructor(
+    private fb: FormBuilder,
+    private vs: ValidatorService,
+    private emailValidator: EmailValidatorService
+  ) {}
 
   ngOnInit(): void {
-    this.miFormulario.setValue({
+    this.miFormulario.reset({
       nombre: 'Jhonatan Soto',
-      email: 'test@test.com',
+      email: 'test1@test.com',
       username: 'Bautistajhonatan45',
+      password: '123456',
+      password2: '123456',
     });
+  }
+
+  get emailErrorMsg(): string {
+    const errors = this.miFormulario.get('email')?.errors;
+
+    if(errors?.['required']){
+      return 'El email es obligatorio'
+    } else if(errors?.['pattern']){
+      return 'El valor no tiene formato de correo electronico.'
+    } else if(errors?.['emailTomado']){
+      return 'El email ya existe.'
+    }
+
+    return '';
   }
 
   campoNoValido(campo: string): boolean | undefined {
@@ -50,6 +72,27 @@ export class RegistroComponent implements OnInit {
       this.miFormulario.get(campo)?.touched
     );
   }
+
+  /*  emailRequired() {
+    return (
+      this.miFormulario.get('email')?.errors?.['required'] &&
+      this.miFormulario.get('email')?.touched
+    );
+  }
+
+  emailPattern() {
+    return (
+      this.miFormulario.get('email')?.errors?.['pattern'] &&
+      this.miFormulario.get('email')?.touched
+    );
+  }
+
+  emailExiste() {
+    return (
+      this.miFormulario.get('email')?.errors?.['emailTomado'] &&
+      this.miFormulario.get('email')?.touched
+    );
+  } */
 
   submitFormulario() {
     console.log(this.miFormulario.value);
